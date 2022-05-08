76e46aa21c817dcd29d14eaa7f6de2b3ae15d318
prashantmagare <prashantmagare3010@gmail.com>
2022-05-08 21:06:16 +0530

    Add: UC1

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Hello World! program
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
